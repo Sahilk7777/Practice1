c3c6438c89eb6be87f906f177bb5bec1cd883b7c
Sahilk7777 <sahilkonkani7777@gmail.com>
2024-03-18 17:53:59 +0530

    added

--- src/test/java/Basic/OrganizationsTest.java
+++ src/test/java/Basic/OrganizationsTest.java
@@ -1,15 +1,34 @@
 package Basic;
 
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import CommonUtils.PropertyFileUtil;
+import CommonUtils.WebDriverUtil;
+
+
+
 public class OrganizationsTest {
 	@Test 
-	public void CreateOrganizationTest()
+	public void CreateOrganizationTest() throws IOException
 	{
-		ChromeDriver driver=new ChromeDriver();
+
+
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("http://localhost:8888/");
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.name("user_password")).sendKeys("admin");
+		driver.findElement(By.id("submitButton")).click();
+	}
+
 		
 		
 	}
-}
+
